e488715224d5c3a2444df96484a952fa27cd7190
Sarah Ortiz <sarahqortiz23@gmail.com>
2026-03-01 18:52:46 -0600

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,18 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
-
+import  initials from '@/lib/assets/initials.png'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Sarah Q. Ortiz`,
+  description: `Resume and portfolio of Sarah Q. Ortiz, a cloud security engineer, architect, and developer.`,
   openGraph: {
-    images: [HOME_OG_IMAGE_URL],
+    images: ["https://github.com/sarah-q-ortiz/sarah-q-ortiz.github.io/tree/main/src/lib/assets/initials.png"],
   },
 };
 
